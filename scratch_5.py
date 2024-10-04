9e571590cdd3e2cc8f08bf98f1b0d46d975ca550
kaushikikansara48 <kkansar1@asu.edu>
2024-10-04 10:18:42 -0700

    initial

--- scratch_5.py
+++ scratch_5.py
@@ -1,10 +1,1 @@
-# Server (UDP)
-import socket
-
-udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_server_socket.bind(('localhost', 12345))
-print('UDP Server listening on port 12345...')
-
-data, addr = udp_server_socket.recvfrom(1024)  # Receive message from client
-print(f"Received from {addr}: {data.decode()}")
-udp_server_socket.sendto(b'Hello, UDP Client', addr)  # Send response
+print("hello github")
